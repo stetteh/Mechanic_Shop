4273a18880d918bf5e5c5876871edd516b5a4d00
Seth Tetteh-Quaye <stettehq@gmail.com>
2016-03-08 13:40:20 -0600

    classes created

---
+++ Mechanic_Shop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mechanic_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shop = new Shop();
+        }
+    }
+}
---
+++ Mechanic_Shop/Shop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mechanic_Shop
+{
+    class Shop
+    {
+        List<string> myShop = new List<string>();
+
+        public void ChangeOil()
+        {
+            Console.WriteLine("Changing oil");
+            return;
+        }
+
+        public void FillUp()
+        {
+            Console.WriteLine("fill up");
+            return;
+        }
+
+    }
+}
